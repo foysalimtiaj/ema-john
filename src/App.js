5cfe99c5e589ef4a635f1ac08fcafd49e80260c6
foysal <foysalimtiaj7@gmail.com>
2023-02-09 11:46:14 +0600

    mmm

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>i am foysal</h1>
+        <h2>full</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
